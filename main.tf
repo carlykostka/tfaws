f6a53551de86ac3d24d303e532e14818745d14b4
carlykostka <77084941+carlykostka@users.noreply.github.com>
2021-10-06 13:43:19 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,134 @@
+provider "aws" { 
+    region = "us-east-2"
+    access_key = "AKIA4JV3TAKUWGSS55ZD"
+    secret_key = "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"
+}
+
+# Create VPC
+resource "aws_vpc" "prod-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "production"
+    }
+}
+
+# Create Internet Gateway 
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.prod-vpc.id
+}
+
+# Create Custom Route Table 
+resource "aws_route_table" "prod-route-table" {
+    vpc_id = aws_vpc.prod-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+    tags = {
+        Name = "Prod"
+    }
+}
+
+# Create Subnet 
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-2a"
+
+    tags = {
+        Name = "prod-subnet"
+    }
+}
+
+# Associate subnet with route table 
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.subnet-1.id
+    route_table_id = aws_route_table.prod-route-table.id
+}
+
+# Create Security Group 
+resource "aws_security_group" "allow_web" {
+    name = "allow_web_traffic"
+    description = "Allow web in bound traffic"
+    vpc_id = aws_vpc.prod-vpc.id
+
+    ingress { 
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+ ingress { 
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+ ingress { 
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_web"
+    }
+}
+
+# Create Network Interface 
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id = aws_subnet.subnet-1.id
+  private_ips = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# Create Elastic IP Address 
+resource "aws_eip" "one" {
+  vpc = true
+  network_interface = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# Create server 
+resource "aws_instance" "web-server-instance" {
+    ami = "ami-00399ec92321828f5"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-2a"
+    key_name = "access"
+
+    network_interface {
+        device_index = 0 
+        network_interface_id = aws_network_interface.web-server-nic.id
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html 
+                EOF
+    tags = {
+        Name = "web-server"
+    }
+}
